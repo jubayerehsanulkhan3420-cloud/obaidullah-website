28e99c9cfc7eb2cad4aec8431450d6b9a50d33fb
jubayerehsanulkhan3420-cloud <jubayerehsanulkhan3420@gmail.com>
2026-09-21 15:55:30 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const button = document.getElementById("learnMoreBtn");
 button.addEventListener("click", function() {
   alert("Welcome to Obaidullah's World!");
 });
+const contactBtn = document.getElementById("contactBtn");
+
+contactBtn.addEventListener("click", function() {
+  alert("Thanks for visiting Obaidullah's website!");
+});
